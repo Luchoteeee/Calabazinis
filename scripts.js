c298cceeba7dd3f63428a0ede2d78be0e00f3e69
Luchoteeee <luchxpersonal@gmail.com>
2025-02-17 13:29:16 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,7 @@
 function saludar() {
     alert("¡Bienvenido a ver nuestros videos de goles y!");
 }
-document.addEventListener("DOMContentLoaded", function () {
-    let sidebar = document.querySelector(".sidebar");
-    if (sidebar) {
-        setTimeout (() => {
-            sidebar.classList.add("visible")
-        }, 500)
-    }   
-});
+
 
 
 
